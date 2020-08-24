fb3f6c6af1ffa3bd53f532dd6e81457690655500
Arefin <60544231+Arefin12345@users.noreply.github.com>
2020-08-24 13:38:11 +0600

    Initial commit

---
+++ src/networking/Networking.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package networking;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Networking {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        system.out.println("Hello");
+    }
+    
+}
